2238eaee5ab27eb31dad0312031cc3ffa0e7f9ce
[ouxiujuan] <ouxiujuan@sinoiov.com>
2019-06-05 16:50:39 +0800

    集成excel导入导出

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.Service.UserService;
 import com.example.demo.entity.TbUserEntity;
+import com.example.demo.entity.UserDto;
+import com.example.demo.excel.ExcelExportUtil;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 @RestController
 public class UserController {
@@ -41,5 +45,24 @@ public class UserController {
     @ApiOperation(value = "查询测试", notes = "查询测试1")
     public TbUserEntity selfSelectById(@RequestParam(value = "id") Integer id){
         return userService.selfSelectById(id);
+    }
+
+    @GetMapping("/export")
+    public void export(){
+        /**
+         * Mock数据，Java对象列表
+         */
+        List<UserDto> shopDTOList = new ArrayList<UserDto>();
+        for (int i = 0; i < 100; i++) {
+            UserDto shop = new UserDto(1,"1","1");
+            shopDTOList.add(shop);
+        }
+        String filePath = "demo-sheet.xls";
+        /**
+         * Excel导出：Object 转换为 Excel
+         */
+        ExcelExportUtil.exportToFile(filePath,shopDTOList);
+
+
     }
 }
---
+++ src/main/java/com/example/demo/entity/UserDto.java
@@ -0,0 +1,56 @@
+package com.example.demo.entity;
+
+
+import com.example.demo.excel.annotation.ExcelField;
+import com.example.demo.excel.annotation.ExcelSheet;
+import org.apache.poi.hssf.util.HSSFColor;
+
+
+@ExcelSheet(name = "用户列表", headColor = HSSFColor.HSSFColorPredefined.LIGHT_GREEN)
+public class UserDto {
+    @ExcelField(name="id")
+    private Integer id;
+    @ExcelField(name="用户名")
+    private String userName;
+    @ExcelField(name="密码")
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserDto(Integer id, String userName, String password) {
+        this.id = id;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
